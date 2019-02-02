43cb47101e9ffc16b5c7e0631bd13266880df116
alikilic <alikilic27@hotmail.com>
2019-02-01 21:21:41 -0500

    Commit third add logout test

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -46,4 +46,15 @@ public class LoginTests {
         Assert.assertEquals(driver.findElement(By.id("ctl00_MainContent_status")).getText(),"Invalid Login or Password.");
 
     }
+
+    @Test
+    public void logOutTest(){
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+
+        driver.findElement(By.id("ctl00_logout")).click();
+
+        Assert.assertEquals(driver.getTitle(),"Web Orders Login");
+    }
 }
